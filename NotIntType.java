6c0f9c69d22e5639cc99bc1055c91574c06d9740
annuagrawal054 <annuagrawal054@gmail.com>
2016-03-23 19:47:41 +0530

    Added files via upload

---
+++ NotIntType.java
@@ -0,0 +1,13 @@
+
+public class NotIntType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		//Both the values is not an integer
+	//	a = 2.3;
+	//	a = -2.0;
+		
+	}
+
+}
